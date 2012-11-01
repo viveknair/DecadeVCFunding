f5d0cd293050ba9c6242a6b58161d39fefcfcbce
Vivek Nair <vnair611@stanford.edu>
2012-10-31 23:40:49 -0700

    Added in the null-less data

--- data/map_reduce/map.js
+++ data/map_reduce/map.js
@@ -1,4 +1,4 @@
 function() {
-  emit({category_code: this.category_code, funded_year : this.funded_year}, 
-       { count: this.raised_amount})
+  emit({category_code: this.category_code, funded_year : this.funded_year, vc_firm : this.vc_name}, 
+       { total_amount: this.raised_amount, number_funding_rounds: 1 })
 }
--- data/map_reduce/reduce.js
+++ data/map_reduce/reduce.js
@@ -1,8 +1,11 @@
 function(key, values) {
-  var result = { total_amount : 0 }
+  var result = { total_amount : 0, number_funding_rounds : 0 }
   values.forEach(function(val) {
-    if (val.count) {  
-      result.total_amount += val.count;
+    if (val.total_amount) {  
+      result.total_amount += val.total_amount;
+    }
+    if (val.number_funding_rounds) {
+      result.number_funding_rounds += val.number_funding_rounds;
     }
   });
   return result;
